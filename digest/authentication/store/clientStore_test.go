1dea7a85cba92af1d701dbbdecdd36d525b0f12e
Nathan McBride <15730096+NathMcBride@users.noreply.github.com>
2024-08-30 15:56:38 +0100

    Add client store tests

---
+++ digest/authentication/store/clientStore_test.go
@@ -0,0 +1,19 @@
+package store_test
+
+import (
+	"github.com/NathMcBride/web-authentication/digest/authentication/store"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Client Store", func() {
+	It("can add and remove clients from the store", func() {
+		clientStore := store.NewClientStore()
+
+		clientStore.Add("an-entry")
+		Expect(clientStore.Has("an-entry")).To(BeTrue())
+
+		clientStore.Delete("an-entry")
+		Expect(clientStore.Has("an-entry")).To(BeFalse())
+	})
+})
